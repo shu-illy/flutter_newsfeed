66450b19fead03ab324b66c7f84462a07521d1b7
Shuhei Iriyama <67177058+shu-illy@users.noreply.github.com>
2021-10-05 23:36:14 +0900

    NewsListViewModel作成

---
+++ lib/viewmodels/news_list_viewmodel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_newsfeed/data/category_info.dart';
+import 'package:flutter_newsfeed/data/search_type.dart';
+
+class NewsListViewModel extends ChangeNotifier {
+  SearchType _searchType = SearchType.CATEGORY;
+  SearchType get searchType => _searchType;
+
+  Category _category = categories[0];
+  Category get category => _category;
+
+  String _keyword = "";
+  String get keyword => _keyword;
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+}
