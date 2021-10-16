3e7740cb2b6fc719ec91e8bccb7807d7d02d74a9
shu-illy <sh.illy.9117@gmail.com>
2021-10-17 03:53:47 +0900

    PageTransformerを使ってページ移動をスムーズにする

--- lib/view/screens/pages/head_line_page.dart
+++ lib/view/screens/pages/head_line_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_newsfeed/data/search_type.dart';
+import 'package:flutter_newsfeed/view/components/page_transformer.dart';
 import 'package:flutter_newsfeed/viewmodels/head_line_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -17,21 +18,31 @@ class HeadLinePage extends StatelessWidget {
       child: Scaffold(
           body: Consumer<HeadLineViewModel>(
             builder: (context, model, child) {
-              return PageView.builder(
-                controller: PageController(),
-                itemCount: model.articles.length,
-                itemBuilder: (context, index) {
-                  final article = model.articles[index];
-                  return Container(
-                    color: Colors.blueAccent,
-                    child: Center(
-                      child: Column(
-                        children: <Widget>[
-                          Text(article.title ?? ""),
-                          Text(article.description ?? ""),
-                        ],
-                      ),
-                    ),
+              return PageTransformer(
+                pageViewBuilder: (context, pageVisibilityResolver) {
+                  return PageView.builder(
+                    controller: PageController(),
+                    itemCount: model.articles.length,
+                    itemBuilder: (context, index) {
+                      final article = model.articles[index];
+                      final pageVisibility =
+                          pageVisibilityResolver.resolvePageVisibility(index);
+                      final visibleFraction = pageVisibility.visibleFraction;
+                      return Opacity(
+                        opacity: visibleFraction,
+                        child: Container(
+                          color: Colors.blueAccent,
+                          child: Center(
+                            child: Column(
+                              children: <Widget>[
+                                Text(article.title ?? ""),
+                                Text(article.description ?? ""),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   );
                 },
               );
