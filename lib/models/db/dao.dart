de404e5bea32dffe2bc545a0a9066fa7def72820
shu-illy <sh.illy.9117@gmail.com>
2021-10-17 17:26:19 +0900

    DBクラスのインスタンス作成

--- lib/models/db/dao.dart
+++ lib/models/db/dao.dart
@@ -6,4 +6,23 @@ part 'dao.g.dart';
 @DriftAccessor(tables: [ArticleRecords])
 class NewsDao extends DatabaseAccessor<MyDatabase> with _$NewsDaoMixin {
   NewsDao(MyDatabase db) : super(db);
+
+  Future clearDB() => delete(articleRecords).go();
+
+  Future insertDB(List<ArticleRecord> articles) async {
+    await batch((batch) {
+      batch.insertAll(articleRecords, articles);
+    });
+  }
+
+  Future<List<ArticleRecord>> get articlesFromDB =>
+      select(articleRecords).get();
+
+  Future<List<ArticleRecord>> insertAndReadNewsFromDB(
+          List<ArticleRecord> articles) =>
+      transaction(() async {
+        await clearDB();
+        await insertDB(articles);
+        return await articlesFromDB;
+      });
 }
