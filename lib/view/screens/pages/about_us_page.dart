af2beba8b159ebde0ed9adfaadb9b9c51c3fb092
shu-illy <sh.illy.9117@gmail.com>
2021-10-21 23:34:39 +0900

    103 about us page

--- lib/view/screens/pages/about_us_page.dart
+++ lib/view/screens/pages/about_us_page.dart
@@ -1,22 +1,42 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class AboutUsPage extends StatefulWidget {
-  const AboutUsPage({Key? key}) : super(key: key);
-
   @override
   _AboutUsPageState createState() => _AboutUsPageState();
 }
 
 class _AboutUsPageState extends State<AboutUsPage> {
+  bool _selected = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         // TODO
-        body: Container(
-          child: Center(
-            child: Text('About Us'),
-          ),
+        body: Center(
+          child: AnimatedContainer(
+              width: _selected ? 400 : 50,
+              height: _selected ? 200 : 25,
+              alignment: _selected ? Alignment.center : Alignment.topCenter,
+              duration: Duration(milliseconds: 1000),
+              curve: Curves.fastOutSlowIn,
+              child: AutoSizeText(
+                "News Feed App",
+                style: TextStyle(fontSize: 40.0),
+                minFontSize: 6.0,
+                maxLines: 1,
+                overflow: TextOverflow.visible,
+              )),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          tooltip: "飛び出すよ",
+          onPressed: () {
+            setState(() {
+              _selected = !_selected;
+            });
+          },
         ),
       ),
     );
